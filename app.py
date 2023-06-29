6044773164c0a4491312b6bd4350c698a9826d8e
Cresence <devharris93@gmail.com>
2023-06-29 14:32:31 -0400

    Switched code from 3rd-party imap site, to redbox gmail connection. Able to successfully pull and read emails.

--- app.py
+++ app.py
@@ -1,71 +1,33 @@
 from dotenv import load_dotenv
 import os
-import imaplib
-import email
-import ssl
+from redbox import gmail
+from redbox.query import SUBJECT, UNSEEN
 
 # Load environment variables from .env file
 load_dotenv()
 
 # dotenv variables + account credentials
-IMAP_SERVER = os.environ.get("email-hostname")
-USERNAME = os.environ.get("email-username")
-PASSWORD = os.environ.get("email-password")
-PORT = os.environ.get("email-port")
+gmail.username = os.environ.get("email-username")
+gmail.password = os.environ.get("email-password")
+gmail.port = os.environ.get("email-port")
 
-# Create an SSL context and disable certificate validation
-context = ssl.create_default_context()
-context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
+gmail.connect()
 
-# Connect to the IMAP server
-imap = imaplib.IMAP4_SSL(IMAP_SERVER, PORT, ssl_context=context)
+inbox = gmail["INBOX"]
 
-# Login to your account
-imap.login(USERNAME, PASSWORD)
+msgs = inbox.search(subject="bill")
 
-# Select the mailbox you want to read emails from (e.g., 'INBOX')
-mailbox = 'INBOX'
-imap.select(mailbox)
+print(type(msgs))
 
-# Search for emails in the selected mailbox
-status, data = imap.search(None, 'ALL')
-email_ids = data[0].split()
+# All message subjects
+for msg in msgs:
+    print(msg.subject)
 
-# Iterate over the email IDs and fetch the email contents
-for email_id in email_ids:
-    # Fetch the email based on the ID
-    status, data = imap.fetch(email_id, '(RFC822)')
-    raw_email = data[0][1]
 
-    # Parse the raw email content
-    msg = email.message_from_bytes(raw_email)
+# Get raw message content
+# print(type(msg.content))
 
-    # Extract email information
-    subject = msg['Subject']
-    sender = msg['From']
-    date = msg['Date']
+# Convert to email.messages.EmailMessage (from standard library)
+# print(type(msg.email))
 
-    # Print email details
-    print(f"Subject: {subject}")
-    print(f"From: {sender}")
-    print(f"Date: {date}")
-
-    # Process email body
-    if msg.is_multipart():
-        # If the email has multiple parts, iterate over them
-        for part in msg.walk():
-            content_type = part.get_content_type()
-            if content_type == 'text/plain':
-                body = part.get_payload(decode=True).decode('utf-8')
-                print(f"Body: {body}")
-    else:
-        # If the email is not multipart, simply extract the body
-        body = msg.get_payload(decode=True).decode('utf-8')
-        print(f"Body: {body}")
-
-    print('-' * 50)
-
-# Logout and close the connection
-imap.logout()
-imap.close()
+gmail.close()
