5e77a32dc1ef78eab5a6c4805ab12e349c0534c3
Cresence <devharris93@gmail.com>
2023-06-28 20:54:42 -0400

    refactored code still giving error: imaplib.IMAP4.abort: command: SELECT => socket error: EOF

--- app.py
+++ app.py
@@ -1,104 +1,71 @@
 from dotenv import load_dotenv
-load_dotenv()
-
+import os
 import imaplib
 import email
-from email.header import decode_header
-import webbrowser
-import os
+import ssl
+
+# Load environment variables from .env file
+load_dotenv()
 
 # dotenv variables + account credentials
-imap_server = os.environ.get("email-hostname")
-username = os.environ.get("email-username")
-password = os.environ.get("email-password")
-port = os.environ.get("email-port")
+IMAP_SERVER = os.environ.get("email-hostname")
+USERNAME = os.environ.get("email-username")
+PASSWORD = os.environ.get("email-password")
+PORT = os.environ.get("email-port")
 
-def clean(text):
-    # clean text for creating a folder
-    return "".join(c if c.isalnum() else "_" for c in text)
+# Create an SSL context and disable certificate validation
+context = ssl.create_default_context()
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
-# create an IMAP4 class with SSL
-imap = imaplib.IMAP4_SSL(imap_server)
+# Connect to the IMAP server
+imap = imaplib.IMAP4_SSL(IMAP_SERVER, PORT, ssl_context=context)
 
-# authenticate
-imap.login(username, password)
+# Login to your account
+imap.login(USERNAME, PASSWORD)
 
-# email fetching (if successful)
-status, messages = imap.select("INBOX")
+# Select the mailbox you want to read emails from (e.g., 'INBOX')
+mailbox = 'INBOX'
+imap.select(mailbox)
 
-# number of top emails to fetch
-N = 3
+# Search for emails in the selected mailbox
+status, data = imap.search(None, 'ALL')
+email_ids = data[0].split()
 
-# total number of emails
-messages = int(messages[0])
+# Iterate over the email IDs and fetch the email contents
+for email_id in email_ids:
+    # Fetch the email based on the ID
+    status, data = imap.fetch(email_id, '(RFC822)')
+    raw_email = data[0][1]
 
-# email loop and extraction
-for i in range(messages, messages-n, -1):
-    # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes eail into a message object
-            msg = email.message_from_bytes(response[1])
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # if it's a bytes, decode to str
-                subject = subject.decode(encoding)
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                From = From.decode(encoding)
-            print("Subject:", subject)
-            print("From:", From)
-            # if the email message is multipart
-            if msg.is_multipart():
-                # iterate over email parts
-                for part in msg.walk():
-                    # extract content type of email
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
-                        print(body)
-                    elif "attachment" in content_disposition:
-                        # download attachment
-                        filename = part.get_filename()
-                        if filename:
-                            folder_name = clean(subject)
-                            if not os.path.isdir(folder_name):
-                                # make a folder for this email (named after the subject)
-                                os.mkdir(folder_name)
-                            filepath = os.path.join(folder_name, filename)
-                            # download attachment and save it
-                            open(filepath, "wb").write(part.get_payload(decode=True))
-            else: 
-                # extract content type of email
-                content_type = msg.get_content_type()
-                # get the email body
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # print only text email parts
-                    print(body)
-                if content_type == "text/html":
-                    # if it's HTML, create a new HTML file and open it in browser
-                    folder_name = clean(subject)
-                    if not os.path.isdir(folder_name):
-                        # make a folder for this email (named after the subject)
-                        os.mkdir(folder_name)
-                    filename = "index.html"
-                    filepath = os.path.join(folder_name, filename)
-                    # write the file
-                    open(filepath, "w").write(body)
-                    # open in the default browser
-                    webbrowser.open(filepath)
-                print("="*100)
-    
-# close the connection and logout
-imap.close()
+    # Parse the raw email content
+    msg = email.message_from_bytes(raw_email)
+
+    # Extract email information
+    subject = msg['Subject']
+    sender = msg['From']
+    date = msg['Date']
+
+    # Print email details
+    print(f"Subject: {subject}")
+    print(f"From: {sender}")
+    print(f"Date: {date}")
+
+    # Process email body
+    if msg.is_multipart():
+        # If the email has multiple parts, iterate over them
+        for part in msg.walk():
+            content_type = part.get_content_type()
+            if content_type == 'text/plain':
+                body = part.get_payload(decode=True).decode('utf-8')
+                print(f"Body: {body}")
+    else:
+        # If the email is not multipart, simply extract the body
+        body = msg.get_payload(decode=True).decode('utf-8')
+        print(f"Body: {body}")
+
+    print('-' * 50)
+
+# Logout and close the connection
 imap.logout()
+imap.close()
